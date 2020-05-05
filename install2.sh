fe180988f1a933ad0f91867cb477d79433990099
rajnikantmahato <58804492+rajnikantmahato@users.noreply.github.com>
2020-05-05 17:07:00 +0530

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -12,7 +12,21 @@ apt install w3m -y
 apt install tor -y
 apt install qemu-common -y
 clear
+mv Aprilfull.sh	
+
+mv extratool.sh
+mv hacker.html	
+mv index.php	
+mv index2.html	
+mv ip.php	
+mv template.php
+
+clear
 
 figlet -f small m a d e   b y
 toilet -f bigmono12 -F gay _RAJNI_
 echo  respect Coders
+echo type Aprilfull.sh to run
+echo not working then type bash Aprilfull.sh to run
+
+
