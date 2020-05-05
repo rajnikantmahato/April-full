0aa497c2620a4167c982db6b09a5f4f2c0b35eb6
rajnikantmahato <58804492+rajnikantmahato@users.noreply.github.com>
2020-05-05 07:34:54 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+clear
+apt install figlet -y
+apt install toilet -y
+apt install php -y
+apt install w3m -y
+apt install tor -y
+apt install qemu-common -y
+clear
+
+figlet -f small m a d e   b y
+toilet -F RAJNI
+echo  respect coders
