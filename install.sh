a918c29e894947af4d4fba4f3b774a67b4bb64ce
rajnikantmahato <58804492+rajnikantmahato@users.noreply.github.com>
2020-05-05 17:09:56 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,23 @@ apt install php -y
 apt install w3m -y
 apt install tor -y
 
+chmod +x *
+mv Aprilfull.sh	/usr/bin/
 
-mv Aprilfull.sh	
+mv extratool.sh /usr/bin/
+mv hacker.html	/usr/bin/
+mv index.php	/usr/bin/
+mv index2.html	/usr/bin/
+mv ip.php	/usr/bin/
+mv template.php /usr/bin/
+
+clear
+
+figlet -f small m a d e   b y
+toilet -f bigmono12 -F gay _RAJNI_
+echo  respect Coders
+echo type Aprilfull.sh to run
+echo not working then type bash Aprilfull.sh
 
 mv extratool.sh
 mv hacker.html	
