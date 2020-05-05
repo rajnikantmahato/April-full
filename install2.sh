587c49257301d8659b5bc3ac5170f2f397f6d9ee
rajnikantmahato <58804492+rajnikantmahato@users.noreply.github.com>
2020-05-05 12:38:14 +0530

    Update and rename install.sh to install2.sh

--- install2.sh
+++ install2.sh
@@ -1,10 +1,16 @@
 clear
+pkg update -y
+pkg upgrade -y
+
+pkg install x11-repo
+pkg install openbox pypanel xorg-xsetroot -y
+pkg install xfce4 -y
 apt install figlet -y
 apt install toilet -y
 apt install php -y
 apt install w3m -y
 apt install tor -y
-pkg install qemu-common -y
+apt install qemu-common -y
 clear
 
 figlet -f small m a d e   b y
