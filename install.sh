2fdcc7a6cc9e2c4df46842429a605bd4861495bc
rajnikantmahato <58804492+rajnikantmahato@users.noreply.github.com>
2020-05-05 12:39:56 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+clear
+apt update -y
+apt upgrade -y
+apt install openbox pypanel xorg-xsetroot -y
+apt install xfce4 -y
+apt install figlet -y
+apt install toilet -y
+apt install php -y
+apt install w3m -y
+apt install tor -y
+
+clear
+
+figlet -f small m a d e   b y
+toilet -f bigmono12 -F gay _RAJNI_
+echo  respect Coders
