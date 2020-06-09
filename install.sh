269595c62293202afd5af4b881f10f2f91e04f95
rajnikantmahato <58804492+rajnikantmahato@users.noreply.github.com>
2020-06-09 20:16:33 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,20 +10,12 @@ apt install w3m -y
 apt install tor -y
 
 chmod +x *
-mv Aprilfull.sh	/usr/bin/
-
-mv hacker.html	/usr/bin/
-mv index.php	/usr/bin/
-mv index2.html	/usr/bin/
-mv ip.php	/usr/bin/
-mv template.php /usr/bin/
 cd
-rm -rf April-full
 clear
 
 figlet -f small m a d e   b y
 toilet -f bigmono12 -F gay _RAJNI_
 echo  respect Coders
-echo type cd /usr/bin
+
 echo type Aprilfull.sh to run
 echo not working then type bash Aprilfull.sh
