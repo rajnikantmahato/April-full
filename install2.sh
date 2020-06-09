e2a5cfdcd4ec08df9ea09fb9c5f9b12da65b0cb9
rajnikantmahato <58804492+rajnikantmahato@users.noreply.github.com>
2020-06-09 20:19:00 +0530

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -12,20 +12,13 @@ apt install w3m -y
 apt install tor -y
 apt install qemu-common -y
 clear
-mv Aprilfull.sh	$PREFIX/bin/
-mv hacker.html	$PREFIX/bin/
-mv index.php	$PREFIX/bin/
-mv index2.html	$PREFIX/bin/
-mv ip.php	$PREFIX/bin/
-mv template.php $PREFIX/bin/
 cd
-rm -rf April-full
 clear
 
 figlet -f small m a d e   b y
 toilet -f bigmono12 -F gay _RAJNI_
 echo  respect Coders
-echo type cd $PREFIX/bin/
+
 echo type Aprilfull.sh to run
 echo not working then type bash Aprilfull.sh to run
 
